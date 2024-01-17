ba97a141a70423902283f704998dbe01e02f3817
aayushbist49 <bistaayush49@gmail.com>
2024-01-17 00:33:12 -0500

    Managed documents

--- javaScript/darkMode/index.js
+++
@@ -1,4 +0,0 @@
-const checkbox = document.getElementById("checkbox");
-checkbox.addEventListener("click", function () {
-    document.body.classList.toggle("darkMode");
-})
--- javaScript/timeAndDate_javaScript/script.js
+++
@@ -1,7 +0,0 @@
-setInterval(() => {
-t= new Date();
-let time = t.getHours() + ":" + t.getMinutes() + ":" + t.getSeconds();
-let date = t.toLocaleDateString();
-document.getElementById('time').innerHTML = time + " on " + date;
-
-}, 1);
